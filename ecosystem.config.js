374c99e5cf66c778f1c4866752c5414b80bf4184
Nuralim <nuralim@rds.co.id>
2026-02-14 03:17:02 +0700

    feat: worker v2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [
         {
-            name: 'disbot-worker',
-            script: './worker/dist/engine.js', // Jalankan file JS hasil build secara langsung
-            node_args: '--max-old-space-size=512', // Ini cara yang benar memberikan RAM ke Node
+            name: 'disbot-v2',
+            script: './worker/dist/engine.js',
+            node_args: '--max-old-space-size=512',
             env: {
                 NODE_ENV: 'production',
             },
-            cwd: './' // Pastikan CWD mengarah ke root project Anda
-        },
+            cwd: './'
+        }
     ],
 };
