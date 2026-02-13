e21d9b4dd444721eadbc9706a6b9d8394ff328f2
Nuralim <nuralim@rds.co.id>
2026-02-14 03:10:55 +0700

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,12 @@ module.exports = {
     apps: [
         {
             name: 'disbot-worker',
-            script: 'npm',
-            args: 'run start -- --max-old-space-size=512',
+            script: './worker/dist/engine.js', // Jalankan file JS hasil build secara langsung
+            node_args: '--max-old-space-size=512', // Ini cara yang benar memberikan RAM ke Node
             env: {
                 NODE_ENV: 'production',
             },
-            cwd: './worker',
-            // Alternatif yang lebih kuat jika npm start tidak meneruskan argumen:
-            // script: './worker/dist/engine.js',
-            // node_args: '--max-old-space-size=512',
+            cwd: './' // Pastikan CWD mengarah ke root project Anda
         },
     ],
 };
