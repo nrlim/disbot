c626e55b5158a65d318bcb25ff07a7288413d04c
Nuralim <nuralim@rds.co.id>
2026-02-15 03:52:44 +0700

    fix: production env

--- src/app/api/payment/create-transaction/route.ts
+++ src/app/api/payment/create-transaction/route.ts
@@ -34,7 +34,7 @@ export async function POST(req: Request) {
 
         // Initialize Snap client
         const snap = new midtransClient.Snap({
-            isProduction: false,
+            isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'true',
             serverKey: process.env.MIDTRANS_SERVER_KEY || "",
             clientKey: process.env.MIDTRANS_CLIENT_KEY || ""
         });
